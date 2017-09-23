d47e98cab18f15a5c321ed76f6d13ebc7665a548
Caleb Dechant <calebdechant@gmail.com>
2017-09-22 18:13:10 -0700

    Update main to demonstrate tls_server, update CMakeLists.txt

--- src/main.cc
+++ src/main.cc
@@ -6,9 +6,12 @@
 
 #include "http_request.h"
 #include "http_response.h"
-#include "plain_socket.h"
+#include "server_socket.h"
 #include "socket.h"
 #include "ssl_manager.h"
+#include "tcp_server.h"
+#include "tcp_socket.h"
+#include "tls_server.h"
 #include "tls_socket.h"
 #include "tokenizer.h"
 
@@ -20,6 +23,35 @@ int main(int argc, char *argv[])
         host = std::string(argv[1]);
     }
     try {
+        if (argc == 1) {
+            cmd::tls_server serv{"cert.pem", "key.pem"};
+            serv.bind(5005);
+            serv.listen(1);
+            cmd::socket::ptr client = serv.accept();
+            cmd::stream stream{client};
+
+            std::string line;
+            while (stream.has_more()) {
+                stream.next_line(line);
+                std::cout << line << "\n";
+                line.clear();
+            }
+
+            exit(1);
+        }
+
+        {
+            cmd::socket::ptr s = cmd::ssl_manager::get_socket_ptr();
+            s->connect(host, 5005);
+            char buf[] = "Hello world\r\n";
+            char buf2[] = "Goodbye world\r\n";
+            s->send(buf, sizeof(buf), 0);
+            s->send(buf2, sizeof(buf2), 0);
+            usleep(1000);
+            s->close();
+        }
+
+        exit(1);
         cmd::http_request r{host};
         r.set_request_method("GET");
         r.connect();
@@ -39,39 +71,6 @@ int main(int argc, char *argv[])
         std::cout << "---------------------------BODY--------------------------\n"
                   << response.body();
         std::cout << "\n---------------------------------------------------------\n";
-
-        response = r.response();
-
-        std::cout << "Status: " << response.status_code() << "\n";
-        std::cout << "-------------------------HEADERS-------------------------\n";
-        for (std::string &s : response.headers()) {
-            std::cout << s << "\n";
-        }
-        std::cout << "---------------------------------------------------------\n";
-
-        std::cout << "---------------------------BODY--------------------------\n"
-                  << response.body();
-        std::cout << "\n---------------------------------------------------------\n";
-
-        exit(0);
-
-        cmd::socket::ptr conn = cmd::ssl_manager::instance().get_socket_ptr();
-        conn->connect("irc.chat.twitch.tv", 443);
-
-        std::string request;
-        request += "PASS oauth:u1vju09sa06pkijp49gnqni25lc7a9\n";
-        request += "NICK exclamation_point_meme\n";
-        request += "join #shroud\njoin #summit1g\n";
-        conn->send(request);
-
-        cmd::stream stream{conn};
-        std::string line;
-        while (stream.has_more()) {
-            line.clear();
-            stream.next_line(line);
-            std::cout << line << "\n";
-        }
-
     } catch (std::exception &e) {
         std::cerr << e.what() << "\n";
         std::exit(1);
